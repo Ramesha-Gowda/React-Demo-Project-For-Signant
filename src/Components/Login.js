9ee74c3d04880bb62c108ad56e7a4346b5c40341
Ramesha-Gowda <ramesha.gowda@brillio.com>
2022-08-01 11:05:59 +0530

    application is db driven now with static id

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,56 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import '../Login.css'
 import { Link } from 'react-router-dom';
 import Home from './Home';
+import axios from 'axios';
 // import usernameIcon from "../assests/akar-icons_person.svg";
 
 function Login() {
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [userInfo,setUserInfo]=useState([]);
 
-    const database = [
-        {
-            username: "Ramesha",
-            password: "password"
-        },
-        {
-            username: "Guest",
-            password: "password"
-        }
-    ];
+    useEffect(()=>{
+        axios.get('/users')
+        .then(res=>{
+            debugger
+            setUserInfo(res.data);
+        })
+    },[])
 
     const errors = {
-        uname: "invalid username",
-        pass: "invalid password"
+        username: "invalid username",
+        password: "invalid password"
     };
 
     const renderErrorMessage = (name) => name === errorMessages.name && (<div className="error">{errorMessages.message}</div>);
 
     const handleSubmit = (event) => {
-        debugger
+        //setErrorMessages to empty intially
+        setErrorMessages({});
         // Prevent page reload
         event.preventDefault();
-        var { uname, pass } = document.forms[0];
+        var { username, password } = document.forms[0];
 
         // Find user login info
-        const userData = database.find((user) => user.username === uname.value);
+        const userData = userInfo.find((user) => user.name === username.value);
 
         // Compare user info
         if (userData) {
-            if (userData.password !== pass.value) {
+            if (userData.password !== password.value) {
                 // Invalid password
-                setErrorMessages({ name: "pass", message: errors.pass });
+                setErrorMessages({ name: "password", message: errors.password });
             } else {
                 setIsSubmitted(true);
             }
         } else {
             // Username not found
-            setErrorMessages({ name: "uname", message: errors.uname });
+            setErrorMessages({ name: "username", message: errors.username });
         }
     };
-
     const toggle = () => {
-        debugger
         var temp = document.getElementById("password");
         if (temp.type === "password") {
             temp.type = "text";
@@ -76,13 +74,13 @@ function Login() {
                             <form onSubmit={handleSubmit}>
                                 <div className="input-container">
                                     <label>Username </label>
-                                    <input id='name' type="text" name="uname" required />
-                                    {renderErrorMessage("uname")}
+                                    <input id='name' type="text" name="username" required />
+                                    {renderErrorMessage("username")}
                                 </div>
                                 <div className="input-container">
                                     <label>Password </label>
-                                    <input id='password' type="password" name="pass" required />
-                                    {renderErrorMessage("pass")}
+                                    <input id='password' type="password" name="password" required />
+                                    {renderErrorMessage("password")}
                                 </div>
                                 <p style={{ marginLeft: '2%' }}>Click on the checkbox to show
                                     or hide password: </p>
