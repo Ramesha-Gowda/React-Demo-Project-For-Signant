a7ab7e658e84ada983620cd089f2f9fa45a299c5
Ramesha-Gowda <ramesha.gowda@brillio.com>
2022-08-02 17:12:58 +0530

    chnages added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import '../App.css';
 import AppNavbar from '../AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
+import { Container } from 'reactstrap';
 import ItemsList from './ItemsList';
 
 const Home = () => {
